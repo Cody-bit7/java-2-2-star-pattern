ca3ea7cd2e7fd043b464a250a99e8e58dcde5420
Cody-bit7 <cjmckeon9@gmail.com>
2025-09-10 08:45:03 -0400

    star pattern
    finished project 2-2

--- StarPattern.java
+++ StarPattern.java
@@ -2,7 +2,12 @@ public class StarPattern {
 
     public static void main(String[] args) {
         // Display the pattern below.
-
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("***********");
+        System.out.println("  *******  ");
+        System.out.println(" **** **** ");
+        System.out.println("**       **");
     }
     
 }
